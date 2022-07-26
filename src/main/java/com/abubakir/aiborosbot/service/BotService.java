011e172e37aaa88f5729273e9d0513e79b691651
joao.abubakir <abubakirjoaopedro@gmail.com>
2022-07-26 20:08:42 -0300

    rework

--- src/main/java/com/abubakir/aiborosbot/service/BotService.java
+++
@@ -1,8 +0,0 @@
-package com.abubakir.aiborosbot.service;
-
-import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@AllArgsConstructor
-public class BotService {}
