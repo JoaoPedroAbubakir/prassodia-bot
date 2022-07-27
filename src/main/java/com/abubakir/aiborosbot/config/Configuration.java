91c6fdeb057050679ee0bf0287c6f5f6f0ba0ae7
joao.abubakir <abubakirjoaopedro@gmail.com>
2022-07-26 22:50:51 -0300

    added music

--- src/main/java/com/abubakir/aiborosbot/config/Configuration.java
+++ src/main/java/com/abubakir/aiborosbot/config/Configuration.java
@@ -1,7 +1,14 @@
 package com.abubakir.aiborosbot.config;
 
+import com.abubakir.aiborosbot.audio.LavaPlayerAudioProvider;
 import com.abubakir.aiborosbot.manager.BotManager;
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
+import com.sedmelluq.discord.lavaplayer.track.playback.NonAllocatingAudioFrameBuffer;
 import discord4j.core.DiscordClient;
+import discord4j.voice.AudioProvider;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -18,8 +25,21 @@ public class Configuration {
     }
 
     @Bean
-    public BotManager botManager(DiscordClient discordClient) {
-        return new BotManager(discordClient);
+    public AudioPlayer audioPlayerManager() {
+        DefaultAudioPlayerManager defaultAudioPlayerManager = new DefaultAudioPlayerManager();
+        defaultAudioPlayerManager.getConfiguration().setFrameBufferFactory(NonAllocatingAudioFrameBuffer::new);
+        AudioSourceManagers.registerRemoteSources(defaultAudioPlayerManager);
+        return defaultAudioPlayerManager.createPlayer();
+    }
+
+    @Bean
+    public AudioProvider audioProvider(AudioPlayer audioPlayer) {
+        return new LavaPlayerAudioProvider(audioPlayer);
+    }
+
+    @Bean
+    public BotManager botManager(DiscordClient discordClient, AudioProvider audioProvider) {
+        return new BotManager(discordClient, audioProvider);
     }
 
 }
--- src/main/java/com/abubakir/aiborosbot/manager/BotManager.java
+++ src/main/java/com/abubakir/aiborosbot/manager/BotManager.java
@@ -1,14 +1,63 @@
 package com.abubakir.aiborosbot.manager;
 
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
 import discord4j.core.DiscordClient;
+import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.Event;
+import discord4j.core.event.domain.VoiceStateUpdateEvent;
+import discord4j.core.event.domain.lifecycle.ReadyEvent;
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.VoiceState;
+import discord4j.core.object.entity.Message;
+import discord4j.voice.AudioProvider;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
 
-@AllArgsConstructor
+import static java.rmi.server.LogStream.log;
+
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Slf4j
 public class BotManager {
 
     DiscordClient client;
+    AudioProvider audioProvider;
+
+    @Builder
+    public BotManager(DiscordClient client, AudioProvider audioProvider) {
+        this.client = client;
+        this.audioProvider = audioProvider;
+        start();
+    }
+
+    public void start() {
+        log.info("start was called");
+        client.withGateway(client -> {
+                    client.getEventDispatcher().on(ReadyEvent.class)
+                            .subscribe(ready -> log.info(String.format("Logged in as %s", ready.getSelf().getUsername())));
+
+                    client.getEventDispatcher().on(MessageCreateEvent.class)
+                            .map(MessageCreateEvent::getMessage)
+                            .filter(msg -> msg.getContent().equals("!ping"))
+                            .flatMap(Message::getChannel)
+                            .flatMap(channel -> channel.createMessage("Pong!"))
+                            .subscribe();
+
+
+
+                    client.getEventDispatcher().on(Event.class)
+                            .subscribe(event -> log.info(String.format("Event type: %s", event.getClass().getName())));
+
+                    return client.onDisconnect();
+                })
+                .block();
+    }
+
+
+
 
 }
